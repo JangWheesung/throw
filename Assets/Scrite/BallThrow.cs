98e5b42f91e941204b2d832641b06e95d77673bf
JangWheesung <gameplay1qaz@gmail.com>
2023-03-22 20:14:14 +0900

    arrowChange

--- Assets/Scrite/BallThrow.cs
+++ Assets/Scrite/BallThrow.cs
@@ -5,12 +5,15 @@ using UnityEngine.EventSystems;
 
 public class BallThrow : MonoBehaviour
 {
+    float flyAngle;
+
     [Header("VectorArrow")]
     [SerializeField] GameObject vectorArrow;
     [SerializeField] float arrowR;
     [SerializeField] float arrowSpeed;
-
     float deg;
+
+    bool canThrow;
 
     private void Awake()
     {
@@ -19,21 +22,27 @@ public class BallThrow : MonoBehaviour
 
     void Update()
     {
-        SetVector();
+        while (true)
+        {
+            flyAngle = SetVector();
+            if (flyAngle != 0) Debug.Log(flyAngle); break;
+        }
     }
 
-    void SetVector()
+    float SetVector()
     {
-        deg += Time.deltaTime * arrowSpeed;
         var rag = Mathf.Deg2Rad * (deg);
-        Debug.Log(rag);
         var x = arrowR * Mathf.Sin(rag);
         var y = arrowR * Mathf.Cos(rag);
         vectorArrow.transform.position = transform.position + new Vector3(x, y);
         vectorArrow.transform.rotation = Quaternion.Euler(0, 0, deg * -1);
 
-        if (Input.GetKey(KeyCode.A)) Debug.Log(333); deg -= Time.deltaTime * arrowSpeed;
-        if (Input.GetKey(KeyCode.D)) deg += Time.deltaTime * arrowSpeed;
+        if (Input.GetKey(KeyCode.A)) deg -= arrowSpeed / 5;
+        if (Input.GetKey(KeyCode.D)) deg += arrowSpeed / 5;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            return vectorArrow.transform.eulerAngles.z;
+        else return 0;
     }
 
     void SetPower()
