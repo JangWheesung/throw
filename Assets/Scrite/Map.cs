98d4eb4c973a3a128a9b202a4a686e44e9ffaaf9
JangWheesung <gameplay1qaz@gmail.com>
2023-03-24 21:58:31 +0900

    stageRootEnd

--- Assets/Scrite/Map.cs
+++ Assets/Scrite/Map.cs
@@ -19,7 +19,7 @@ public class Map : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) ground.SetActive(false);
 
-        if (player.transform.position.y >= 160)
+        if (player.transform.position.y >= 165)
         {
             CreateMap();
 
@@ -34,31 +34,38 @@ public class Map : MonoBehaviour
         if (firstCreatMap)
         {
             //InitObject();
-
+            int[] j = new int[16];
             for (int i = 3; i < 16; i++)
             {
-                poolingFrefab[i].transform.position = new Vector2(Random.Range(20, -20), i * 10);
+                int randomPos = Random.Range(20, -20);
+                for (; randomPos >= j[i - 1] - 7 && randomPos <= j[i - 1] + 7;)
+                {
+                    randomPos = Random.Range(20, -20);
+                }
+                j[i] = randomPos;
+
+                poolingFrefab[i].transform.position = new Vector2(randomPos, i * 10);
                 poolingFrefab[i].transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+                //CreatePos(i, out j);
             }
         }
         else
         {
+            int[] j = new int[16];
             for (int i = 30; i < 160; i += 10)
             {
-                Vector2 popPos = new Vector2(Random.Range(20, -20), i);
+                int randomPos = Random.Range(20, -20);
+                for (; randomPos >= j[(i / 10) - 1] - 7 && randomPos <= j[(i / 10) - 1] + 7;)
+                {
+                    randomPos = Random.Range(20, -20);
+                }
+                j[(i / 10)] = randomPos;
+
+                Vector2 popPos = new Vector2(randomPos, i);
                 Quaternion popAngle = Quaternion.Euler(0, 0, Random.Range(0, 360));
                 poolingFrefab[i/10] = Instantiate(prefab[Random.Range(0, prefab.Length)], popPos, popAngle);
             }
         }
         firstCreatMap = true;
-    }
-
-    void InitObject()
-    {
-        GameObject[] clone = new GameObject[16];
-        for (int i = 3; i < 16; i++)
-        {
-            poolingFrefab[i] = clone[Random.Range(3, 15)];
-        }
     }
 }
